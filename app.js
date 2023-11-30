d0989be596e8e32ffcbae6f3d57a48a62a4b5366
youssefsaad22 <saadllaouiyoussef@gmail.com>
2023-11-30 15:35:16 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+//console.log(window.document);
+
+//recuperation de l'élément input par son id firstcolor
+const firstcolorElt = document.getElementById('firstColor');
+//console.log(firstcolorElt);
+
+//recuperation de l'élément input par son id secondecolor
+const secondecolorElt = document.getElementById('secondColor');
+//console.log(secondecolorElt);
+
+//recuperation de l'élément body
+const bodyElt= document.querySelector('body');
+//console.log(bodyElt);
+
+
+firstcolorElt.addEventListener('input',function() {
+    //console.log(firstcolorElt.value);
+    bodyElt.style="background: linear-gradient(to right,"+firstcolorElt.value+","+secondecolorElt.value+");";
+});
+
+secondecolorElt.addEventListener('input',function() {
+    //console.log(secondecolorElt.value);
+    //bodyElt.style="background: linear-gradient(to right,"+firstcolorElt.value+","+secondecolorElt.value+");";
+    bodyElt.style=`background: linear-gradient(to right,${firstcolorElt.value},${secondecolorElt.value});`
+});
